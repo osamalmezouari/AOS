3d902cfcdd698c802d3c643493ffb95ee64a95cc
oussama <oussamalm147@gmail.com>
2024-04-08 12:15:13 +0000

    fix the login button issue

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React  from "react";
 import "./Navbar.css";
 import logo from "../../Assets/logo.png";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
@@ -10,57 +10,58 @@ export const Navbare = () => {
       {/* Apply inline style */}
       {/* hi  */}
       <div className="navbarr ">
-        <Navbar expand="lg" bg="light" style={{ direction: "rtl" }}>
-          <Container>
-            <div className="nav-logo ">
-              <Navbar.Brand
-                href="#home"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <img
-                  src={logo}
-                  alt=""
-                  style={{
-                    backgroundSize: "cover",
-                    height: "30px",
-                    width: "30px",
-                    backgroundPosition: "center",
-                  }}
+        <Navbar expand="lg" bg="light" style={{ direction: "rtl" }} >
+            <Container className={'gap-2'}>
+                <div className="nav-logo ">
+                    <Navbar.Brand
+                        href="#home"
+                        style={{display: "flex", alignItems: "center"}}
+
+                    >
+                        <img
+                            src={logo}
+                            alt=""
+                            style={{
+                                backgroundSize: "cover",
+                                height: "45px",
+                                width: "45px",
+                                backgroundPosition: "center",
+                            }}
+                        />
+                    </Navbar.Brand>
+                </div>
+
+                <Navbar.Toggle
+                    aria-controls="responsive-navbar-nav"
+                    style={{marginRight: "auto"}}
                 />
-        <div className="ml-auto">
-          <Button variant="success" style={{ width: "120px" }}>تسجيل الدخول</Button>
-        </div>
-              </Navbar.Brand>
-            </div>
 
-            <Navbar.Toggle
-              aria-controls="responsive-navbar-nav"
-              style={{ marginRight: "auto" }}
-            />
-
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <div className="navbar-menu">
-                <Nav className="me-auto" >
-                  <Nav.Link href="#Services" className="me-4">
-                    {" "}
-                    المساعدات المالية
-                  </Nav.Link>
-                  <Nav.Link href="#Works" className="me-4">
-                    {" "}
-                    الضمان والمساعدة الطبية
-                  </Nav.Link>
-                  <Nav.Link href="#Resume" className="me-4">
-                    {" "}
-                    الانشطة الثقافية
-                  </Nav.Link>
-                  <Nav.Link href="#Skills" className="me-4">
-                    {" "}
-                    الإتفاقيات والشراكات{" "}
-                  </Nav.Link>
-                </Nav>
-              </div>
-            </Navbar.Collapse>
-          </Container>{" "}
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <div className="navbar-menu">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#Services" className="me-4">
+                                {" "}
+                                المساعدات المالية
+                            </Nav.Link>
+                            <Nav.Link href="#Works" className="me-4">
+                                {" "}
+                                الضمان والمساعدة الطبية
+                            </Nav.Link>
+                            <Nav.Link href="#Resume" className="me-4">
+                                {" "}
+                                الانشطة الثقافية
+                            </Nav.Link>
+                            <Nav.Link href="#Skills" className="me-4">
+                                {" "}
+                                الإتفاقيات والشراكات{" "}
+                            </Nav.Link>
+                        </Nav>
+                    </div>
+                </Navbar.Collapse>
+                <div className="ml-auto">
+                    <Button variant="success" style={{width: "120px"}}>تسجيل الدخول</Button>
+                </div>
+            </Container>{" "}
         </Navbar>{" "}
       </div>
     </div>
